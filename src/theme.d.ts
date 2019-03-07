b75ad4ca103a38b6bf0ca38593bc1ae3a97f4b87
Andreas Sahle <andreas.sahle@gmail.com>
2019-03-07 20:08:20 +0100

    style headlines with responsive font-sizes

--- src/theme.d.ts
+++ src/theme.d.ts
@@ -1,3 +1,194 @@
+export declare const breakpoints: {
+  [key: string]: string;
+};
+export declare const mediaQueries: {
+  [key: string]: string;
+};
+export declare const colors: {
+  aqua: string;
+  black: string;
+  blue: string;
+  fuchsia: string;
+  gray: string;
+  green: string;
+  lime: string;
+  maroon: string;
+  navy: string;
+  olive: string;
+  orange: string;
+  purple: string;
+  red: string;
+  silver: string;
+  teal: string;
+  white: string;
+  yellow: string;
+};
+export declare const space: string[];
+export declare const fonts: {
+  headline: string;
+  text: string;
+  mono: string;
+};
+export declare const fontSizes: string[];
+export declare const medium = 500;
+export declare const bold = 700;
+export declare const regular = 400;
+export declare const fontWeights: {
+  medium: number;
+  bold: number;
+  regular: number;
+};
+export declare const lineHeights: {
+  standard: number;
+  display: number;
+};
+export declare const letterSpacings: {
+  normal: string;
+  caps: string;
+};
+export declare const textStyles: {
+  h6: {
+    fontFamily: string;
+    fontSize: string;
+    fontWeight: number;
+    lineHeight: number;
+    marginTop: string;
+    marginBottom: string;
+    '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+      marginTop: string;
+    };
+  };
+  h5: {
+    [x: string]:
+      | string
+      | number
+      | {
+          marginTop: string;
+          fontSize?: undefined;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+        }
+      | {
+          fontSize: string;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+            marginTop: string;
+          };
+          marginTop?: undefined;
+        };
+    fontFamily: string;
+    fontSize: string;
+    fontWeight: number;
+    lineHeight: number;
+    marginBottom: string;
+    marginTop: string;
+    '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+      marginTop: string;
+    };
+  };
+  h4: {
+    [x: string]:
+      | string
+      | number
+      | {
+          marginTop: string;
+          fontSize?: undefined;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+        }
+      | {
+          fontSize: string;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+            marginTop: string;
+          };
+          marginTop?: undefined;
+        };
+    fontFamily: string;
+    fontSize: string;
+    fontWeight: number;
+    lineHeight: number;
+    marginBottom: string;
+    marginTop: string;
+    '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+      marginTop: string;
+    };
+  };
+  h3: {
+    [x: string]:
+      | string
+      | number
+      | {
+          marginTop: string;
+          fontSize?: undefined;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+        }
+      | {
+          fontSize: string;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+            marginTop: string;
+          };
+          marginTop?: undefined;
+        };
+    fontFamily: string;
+    fontSize: string;
+    fontWeight: number;
+    lineHeight: number;
+    marginBottom: string;
+    marginTop: string;
+    '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+      marginTop: string;
+    };
+  };
+  h2: {
+    [x: string]:
+      | string
+      | number
+      | {
+          marginTop: string;
+          fontSize?: undefined;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+        }
+      | {
+          fontSize: string;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+            marginTop: string;
+          };
+          marginTop?: undefined;
+        };
+    fontFamily: string;
+    fontSize: string;
+    fontWeight: number;
+    lineHeight: number;
+    marginBottom: string;
+    marginTop: string;
+    '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+      marginTop: string;
+    };
+  };
+  h1: {
+    [x: string]:
+      | string
+      | number
+      | {
+          marginTop: string;
+          fontSize?: undefined;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+        }
+      | {
+          fontSize: string;
+          '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+            marginTop: string;
+          };
+          marginTop?: undefined;
+        };
+    fontFamily: string;
+    fontSize: string;
+    fontWeight: number;
+    lineHeight: number;
+    marginBottom: string;
+    marginTop: string;
+    '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+      marginTop: string;
+    };
+  };
+};
 export declare const defaultTheme: {
   buttons: {
     primary: {
@@ -5,6 +196,149 @@ export declare const defaultTheme: {
       backgroundColor: string;
       '&:hover': {
         backgroundColor: string;
+      };
+    };
+  };
+  textStyles: {
+    h6: {
+      fontFamily: string;
+      fontSize: string;
+      fontWeight: number;
+      lineHeight: number;
+      marginTop: string;
+      marginBottom: string;
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: string;
+      };
+    };
+    h5: {
+      [x: string]:
+        | string
+        | number
+        | {
+            marginTop: string;
+            fontSize?: undefined;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+          }
+        | {
+            fontSize: string;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+              marginTop: string;
+            };
+            marginTop?: undefined;
+          };
+      fontFamily: string;
+      fontSize: string;
+      fontWeight: number;
+      lineHeight: number;
+      marginBottom: string;
+      marginTop: string;
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: string;
+      };
+    };
+    h4: {
+      [x: string]:
+        | string
+        | number
+        | {
+            marginTop: string;
+            fontSize?: undefined;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+          }
+        | {
+            fontSize: string;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+              marginTop: string;
+            };
+            marginTop?: undefined;
+          };
+      fontFamily: string;
+      fontSize: string;
+      fontWeight: number;
+      lineHeight: number;
+      marginBottom: string;
+      marginTop: string;
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: string;
+      };
+    };
+    h3: {
+      [x: string]:
+        | string
+        | number
+        | {
+            marginTop: string;
+            fontSize?: undefined;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+          }
+        | {
+            fontSize: string;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+              marginTop: string;
+            };
+            marginTop?: undefined;
+          };
+      fontFamily: string;
+      fontSize: string;
+      fontWeight: number;
+      lineHeight: number;
+      marginBottom: string;
+      marginTop: string;
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: string;
+      };
+    };
+    h2: {
+      [x: string]:
+        | string
+        | number
+        | {
+            marginTop: string;
+            fontSize?: undefined;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+          }
+        | {
+            fontSize: string;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+              marginTop: string;
+            };
+            marginTop?: undefined;
+          };
+      fontFamily: string;
+      fontSize: string;
+      fontWeight: number;
+      lineHeight: number;
+      marginBottom: string;
+      marginTop: string;
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: string;
+      };
+    };
+    h1: {
+      [x: string]:
+        | string
+        | number
+        | {
+            marginTop: string;
+            fontSize?: undefined;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6'?: undefined;
+          }
+        | {
+            fontSize: string;
+            '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+              marginTop: string;
+            };
+            marginTop?: undefined;
+          };
+      fontFamily: string;
+      fontSize: string;
+      fontWeight: number;
+      lineHeight: number;
+      marginBottom: string;
+      marginTop: string;
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: string;
       };
     };
   };
@@ -40,6 +374,10 @@ export declare const defaultTheme: {
     yellow: string;
   };
   fontSizes: string[];
-  space: Array<string | number>;
+  space: string[];
+  letterSpacings: {
+    normal: string;
+    caps: string;
+  };
 };
 export default defaultTheme;
--- src/theme.ts
+++ src/theme.ts
@@ -1,26 +1,29 @@
 // tslint:disable: object-literal-sort-keys
 const createMediaQuery = (n: string) => `@media screen and (min-width:${n})`;
 
-const addAliases = (arr: string[] | number[], arrAlias: string[]) =>
-  aliases.forEach((key, i) =>
-    Object.defineProperty(arr, key, {
-      enumerable: false,
-      get() {
-        return this[i];
-      },
-    })
-  );
-
-export const breakpoints: string[] = [32, 40, 48, 64].map(
+const aliases = ['sm', 'md', 'lg', 'xl', 'xxl'];
+const breakpointStrings: string[] = [32, 40, 48, 64, 75].map(
   (n: number) => n + 'em'
 );
+export const breakpoints: {
+  [key: string]: string;
+} = breakpointStrings.reduce(
+  (acc, curr, index) => ({
+    ...acc,
+    [`${aliases[index]}`]: curr,
+  }),
+  {}
+);
 
-export const mediaQueries = breakpoints.map(createMediaQuery);
-
-const aliases = ['sm', 'md', 'lg', 'xl'];
-
-addAliases(breakpoints, aliases);
-addAliases(mediaQueries, aliases);
+export const mediaQueries: {
+  [key: string]: string;
+} = breakpointStrings.map(createMediaQuery).reduce(
+  (acc, curr, index) => ({
+    ...acc,
+    [`${aliases[index]}`]: curr,
+  }),
+  {}
+);
 
 export const colors = {
   /* Colors (most courtesy of http://clrs.cc/) */
@@ -56,26 +59,33 @@ export const space = [
 ];
 
 export const fonts = {
-  headline: `'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif`,
-  text: `'Montserrat','Helvetica Neue',Helvetica,Arial,sans-serif`,
+  headline: `system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+  text: `system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+  mono: `Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace`,
 };
 
 export const fontSizes = [
-  '0.625rem',
-  '0.75rem',
-  '0.875rem',
-  '1rem',
-  '1.125rem',
-  '1.5rem',
-  '2rem',
-  '3rem',
-  '4rem',
+  '0.625rem', // 0: 10px
+  '0.75rem', // 1: 12px
+  '0.875rem', // 2: 14px
+  '1rem', // 3: 16px
+  '1.0625rem', // 4: 17px
+  '1.125rem', // 5: 18px
+  '1.1875rem', // 6: 19px
+  '1.25rem', // 7: 20px
+  '1.5rem', // 8: 24px
+  '1.5625rem', // 9: 25px
+  '1.9375rem', // 10: 31px
+  '2rem', // 11: 32px
+  '2.5rem', // 12: 40px
+  '3rem', // 13: 48px
+  '4rem', // 14: 64px
 ];
 
 export const medium = 500;
 export const bold = 700;
 // alias
-export const regular = medium;
+export const regular = 400;
 
 // styled-system's `fontWeight` function can hook into the `fontWeights` object
 export const fontWeights = {
@@ -86,7 +96,7 @@ export const fontWeights = {
 };
 
 export const lineHeights = {
-  standard: 1.5,
+  standard: 1.6,
   display: 1.25,
 };
 
@@ -104,62 +114,92 @@ export const textStyles = {
     marginTop: '0.2em',
     marginBottom: '0.7em',
     '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
-      marginTop: `calc(${fontSizes[4]} * -0.7)`,
+      marginTop: `calc(${fontSizes[3]} * -0.7)`,
     },
   },
   h5: {
     fontFamily: fonts.headline,
     fontSize: fontSizes[4],
-    fontWeight: fontWeights.bold,
+    fontWeight: fontWeights.regular,
     lineHeight: lineHeights.display,
     marginBottom: '0.7em',
     marginTop: '0.2em',
     '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
       marginTop: `calc(${fontSizes[4]} * -0.7)`,
     },
+    [`${mediaQueries.lg}`]: {
+      fontSize: fontSizes[7],
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: `calc(${fontSizes[7]} * -0.7)`,
+      },
+    },
   },
   h4: {
     fontFamily: fonts.headline,
     fontSize: fontSizes[5],
-    fontWeight: fontWeights.bold,
+    fontWeight: fontWeights.regular,
     lineHeight: lineHeights.display,
     marginBottom: '0.7em',
     marginTop: '0.2em',
     '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
       marginTop: `calc(${fontSizes[5]} * -0.7)`,
     },
+    [`${mediaQueries.lg}`]: {
+      fontSize: fontSizes[9],
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: `calc(${fontSizes[9]} * -0.7)`,
+      },
+    },
   },
   h3: {
     fontFamily: fonts.headline,
     fontSize: fontSizes[6],
-    fontWeight: fontWeights.bold,
+    fontWeight: fontWeights.regular,
     lineHeight: lineHeights.display,
     marginBottom: '0.7em',
     marginTop: '0.2em',
     '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
       marginTop: `calc(${fontSizes[6]} * -0.7)`,
     },
+    [`${mediaQueries.lg}`]: {
+      fontSize: fontSizes[10],
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: `calc(${fontSizes[10]} * -0.7)`,
+      },
+    },
   },
   h2: {
     fontFamily: fonts.headline,
     fontSize: fontSizes[7],
-    fontWeight: fontWeights.bold,
+    fontWeight: fontWeights.regular,
     lineHeight: lineHeights.display,
     marginBottom: '0.7em',
     marginTop: '0.2em',
     '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
       marginTop: `calc(${fontSizes[7]} * -0.7)`,
     },
+    [`${mediaQueries.lg}`]: {
+      fontSize: fontSizes[12],
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: `calc(${fontSizes[12]} * -0.7)`,
+      },
+    },
   },
   h1: {
     fontFamily: fonts.headline,
     fontSize: fontSizes[8],
-    fontWeight: fontWeights.bold,
+    fontWeight: fontWeights.regular,
     lineHeight: lineHeights.display,
     marginBottom: '0.7em',
     marginTop: '0.2em',
     '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
       marginTop: `calc(${fontSizes[8]} * -0.7)`,
+    },
+    [`${mediaQueries.lg}`]: {
+      fontSize: fontSizes[13],
+      '& + h1, & + h2, & + h3, & + h4, & + h5, & + h6': {
+        marginTop: `calc(${fontSizes[13]} * -0.7)`,
+      },
     },
   },
 };
