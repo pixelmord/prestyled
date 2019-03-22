7aa4d955b6dfdce18db8631048364244da90a3b9
Andreas Sahle <andreas.sahle@gmail.com>
2019-03-22 16:39:56 +0100

    fix: use p tag for Paragraph

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -6,14 +6,14 @@ import { Theme } from '../../theme';
 import { ITextProps, Text } from '../Text';
 
 export const Paragraph: StyledComponent<
-  PropsWithoutRef<JSX.IntrinsicElements['div']>,
+  PropsWithoutRef<JSX.IntrinsicElements['p']>,
   ITextProps,
   Theme
 > = styled(Text)`
   font-family: ${themeGet('fonts.text', 'sans-serif')};
   line-height: ${themeGet('lineHeights.standard', '1.6')};
   color: ${themeGet('colors.text', 'black')};
-`;
+`.withComponent('p');
 
 Paragraph.defaultProps = {
   mb: 2,
