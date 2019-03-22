e4837afd101362d70ba6a8edf62195799767b81e
Andreas Sahle <andreas.sahle@gmail.com>
2019-03-22 09:06:54 +0100

    chore: config distributable

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export * from './theme';
+
+export * from './components/Box';
+export * from './components/Button';
+export * from './components/Flex';
+export * from './components/Form';
+export * from './components/FormElement';
+export * from './components/FormFieldDescription';
+export * from './components/FormFieldMessage';
+export * from './components/GlobalStyle';
+export * from './components/Headline';
+export * from './components/Input';
+export * from './components/InputLabel';
+export * from './components/Message';
+export * from './components/Paragraph';
+export * from './components/Text';
+export * from './components/TextField';
