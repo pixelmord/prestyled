1f1407fc124a5db35dabe75bd4dde1c746ab4156
Andreas Sahle <andreas.sahle@gmail.com>
2020-01-23 06:20:55 +0100

    fix(component): type

--- src/components/FormElement/FormElement.tsx
+++ src/components/FormElement/FormElement.tsx
@@ -1,9 +1,5 @@
 import styled, { StyledComponent } from '@emotion/styled';
-import { HTMLProps, PropsWithoutRef } from 'react';
+import { PropsWithoutRef } from 'react';
 import { PrestyledTheme as Theme } from '../../theme/index';
 
-export const FormElement: StyledComponent<
-  PropsWithoutRef<JSX.IntrinsicElements['div']>,
-  HTMLProps<HTMLDivElement>,
-  Theme
-> = styled('div')``;
+export const FormElement: StyledComponent<PropsWithoutRef<JSX.IntrinsicElements['div']>, {}, Theme> = styled('div')``;
